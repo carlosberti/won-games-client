e725432e116ab62947cb8c6d4b6bef2678b53f6b
Carlos Berti <carlosberti182@gmail.com>
2020-09-06 15:59:03 -0300

    Add themeprovider to project and create its declaration file

--- .storybook/withGlobalStyles.tsx
+++ .storybook/withGlobalStyles.tsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from 'styled-components'
 import { StoryFn } from '@storybook/addons'
 import GlobalStyles from '../src/styles/global'
+import theme from '../src/styles/theme'
 
 const withGlobalStyles = (storyFn: StoryFn) => (
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyles />
     {storyFn()}
-  </>
+  </ThemeProvider>
 )
 
 export default withGlobalStyles
