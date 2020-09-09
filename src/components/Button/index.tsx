4bf85aee79ea2a5eb3c1255d45e7d16b6f1ed916
Carlos Berti <carlosberti182@gmail.com>
2020-09-08 23:57:59 -0300

    Add button version with icon

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,14 +4,19 @@ export type ButtonProps = {
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  icon?: React.ReactNode
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const Button = ({
   children,
+  icon,
   size = 'medium',
-  fullWidth = false
+  fullWidth = false,
+  ...props
 }: ButtonProps) => (
-  <s.Wrapper size={size} fullWidth={fullWidth}>
+  <s.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+    {icon}
     {!!children && <span>{children}</span>}
   </s.Wrapper>
 )
