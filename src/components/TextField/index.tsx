0f7a1e622d5b0a28df75f15f498d8a3aa5e462ef
Carlos Berti <carlosberti182@gmail.com>
2021-01-16 02:18:44 -0300

    refactor: use name instead of labelfor

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -5,7 +5,6 @@ import * as S from './styles'
 export type TextFieldProps = {
   onInput?: (value: string) => void
   label?: string
-  labelFor?: string
   initialValue?: string
   icon?: React.ReactNode
   iconPosition?: 'left' | 'right'
@@ -16,7 +15,7 @@ export type TextFieldProps = {
 const TextField = ({
   onInput,
   label,
-  labelFor = '',
+  name,
   initialValue = '',
   icon,
   iconPosition = 'left',
@@ -35,15 +34,17 @@ const TextField = ({
 
   return (
     <S.Wrapper disabled={disabled} error={!!error}>
-      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      {!!label && <S.Label htmlFor={name}>{label}</S.Label>}
       <S.InputWrapper>
         {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
         <S.Input
           type="text"
+          name={name}
           onChange={onChange}
           value={value}
           iconPosition={iconPosition}
           disabled={disabled}
+          {...(label ? { id: name } : {})}
           {...props}
         />
       </S.InputWrapper>
