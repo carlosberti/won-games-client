0858c4e4ce37dafb77afbdaf85fd5279bd301d6d
Carlos Berti <carlosberti182@gmail.com>
2021-03-01 00:04:11 -0300

    feat: create userdropdown

---
+++ src/components/UserDropdown/index.tsx
@@ -0,0 +1,49 @@
+import { ChevronDown } from '@styled-icons/boxicons-regular'
+import {
+  AccountCircle,
+  FavoriteBorder,
+  ExitToApp
+} from '@styled-icons/material-outlined'
+import Dropdown from 'components/Dropdown'
+import Link from 'next/link'
+
+import * as s from './styles'
+
+export type UserDropdownProps = {
+  username: string
+}
+
+const UserDropdown = ({ username }: UserDropdownProps) => (
+  <Dropdown
+    title={
+      <>
+        <AccountCircle size={24} />
+        <s.Username>{username}</s.Username>
+        <ChevronDown size={24} />
+      </>
+    }
+  >
+    <s.Nav>
+      <Link href="/profile/me" passHref>
+        <s.Link>
+          <AccountCircle />
+          <span>My profile</span>
+        </s.Link>
+      </Link>
+      <Link href="/wishlist" passHref>
+        <s.Link>
+          <FavoriteBorder />
+          <span>Wishlist</span>
+        </s.Link>
+      </Link>
+      <Link href="/logout" passHref>
+        <s.Link>
+          <ExitToApp />
+          <span>Sign out</span>
+        </s.Link>
+      </Link>
+    </s.Nav>
+  </Dropdown>
+)
+
+export default UserDropdown
