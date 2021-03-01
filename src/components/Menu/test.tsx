316bc26bf01c46c7ea540e1fda0fe73c9c83277a
Carlos Berti <carlosberti182@gmail.com>
2021-03-01 00:24:38 -0300

    test: add new dropdowns to menu

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -9,8 +9,8 @@ describe('<Menu />', () => {
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/Open Shopping Cart/i)).toBeInTheDocument()
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+    expect(screen.getAllByLabelText(/shopping cart/i)).toHaveLength(2)
   })
 
   it('should render the open/close mobile menu', () => {
@@ -39,15 +39,15 @@ describe('<Menu />', () => {
 
     expect(screen.getByText(/Sign in/i)).toBeInTheDocument()
     expect(screen.getByText(/sign up/i)).toBeInTheDocument()
-    expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/my profile/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
   })
 
   it('should show wishlist and account when logged in', () => {
     renderWithTheme(<Menu username="Carlos" />)
 
-    expect(screen.getByText(/my account/i)).toBeInTheDocument()
-    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/my profile/i)).toHaveLength(2)
+    expect(screen.getAllByText(/wishlist/i)).toHaveLength(2)
     expect(screen.queryByText(/Sign in/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
   })
