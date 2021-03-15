84e22ac8644df82c9ddc823ae76dfa4ec31474ad
Carlos Berti <carlosberti182@gmail.com>
2021-03-15 01:53:12 -0300

    test: add new rule to onFilter

--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -69,8 +69,6 @@ describe('<ExploreSidebar />', () => {
       />
     )
 
-    userEvent.click(screen.getByRole('button', { name: /filter/i }))
-
     expect(onFilter).toBeCalledWith({
       platforms: ['windows'],
       sort_by: 'low-to-high'
@@ -86,8 +84,7 @@ describe('<ExploreSidebar />', () => {
     userEvent.click(screen.getByLabelText(/linux/i))
     userEvent.click(screen.getByLabelText(/low to high/i))
 
-    userEvent.click(screen.getByRole('button', { name: /filter/i }))
-
+    expect(onFilter).toHaveBeenCalledTimes(4)
     expect(onFilter).toBeCalledWith({
       platforms: ['windows', 'linux'],
       sort_by: 'low-to-high'
@@ -101,8 +98,6 @@ describe('<ExploreSidebar />', () => {
 
     userEvent.click(screen.getByLabelText(/low to high/i))
     userEvent.click(screen.getByLabelText(/high to low/i))
-
-    userEvent.click(screen.getByRole('button', { name: /filter/i }))
 
     expect(onFilter).toBeCalledWith({ sort_by: 'high-to-low' })
   })
@@ -130,5 +125,9 @@ describe('<ExploreSidebar />', () => {
     userEvent.click(screen.getByLabelText(/close filters/))
 
     expect(Element).not.toHaveStyleRule('opacity', '1', variant)
+
+    userEvent.click(screen.getByLabelText(/open filters/))
+
+    userEvent.click(screen.getByRole('button', { name: /filter/i }))
   })
 })
