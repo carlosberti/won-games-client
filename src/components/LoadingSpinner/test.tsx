062b2f6533c205fc2914aa1f25d85c61e04ad94d
Carlos Berti <carlosberti182@gmail.com>
2021-03-25 02:08:46 -0300

    test: add loading spinner to show more

--- src/components/LoadingSpinner/test.tsx
+++ src/components/LoadingSpinner/test.tsx
@@ -8,7 +8,7 @@ describe('<LoadingSpinner />', () => {
 
     expect(container.firstChild).toHaveStyle({
       borderRadius: '50%',
-      border: '0.6rem solid #030517',
+      border: '0.4rem solid #030517',
       borderLeftColor: '#F231A5'
     })
   })
