7e75e9377c2a55b980f176a71c9622822bac6ed3
Carlos Berti <carlosberti182@gmail.com>
2021-03-28 18:24:32 -0300

    feat: add useCart to cartlist

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,40 +1,43 @@
 import Button from 'components/Button'
 import Empty from 'components/Empty'
-import GameItem, { GameItemProps } from 'components/GameItem'
+import GameItem from 'components/GameItem'
+import { useCart } from 'hooks/use-cart'
 import Link from 'next/link'
 import * as s from './styles'
 
 export type CartListProps = {
-  items?: GameItemProps[]
-  total?: string
   hasButton?: boolean
 }
 
-const CartList = ({ items = [], total, hasButton = false }: CartListProps) => (
-  <s.Wrapper isEmpty={!items.length}>
-    {items.length ? (
-      <>
-        {items.map((item) => (
-          <GameItem key={item.title} {...item} />
-        ))}
+const CartList = ({ hasButton = false }: CartListProps) => {
+  const { items, total } = useCart()
 
-        <s.Footer>
-          {!hasButton && <span>Total:</span>}
-          <s.Total>{total}</s.Total>
-          {hasButton && (
-            <Link href="/cart">
-              <Button as="a">Buy it now</Button>
-            </Link>
-          )}
-        </s.Footer>
-      </>
-    ) : (
-      <Empty
-        title="Your cart is empty"
-        description="Go back to the store and explore great games and offers"
-        hasLink
-      />
-    )}
-  </s.Wrapper>
-)
+  return (
+    <s.Wrapper isEmpty={!items.length}>
+      {items.length ? (
+        <>
+          {items.map((item) => (
+            <GameItem key={item.title} {...item} />
+          ))}
+
+          <s.Footer>
+            {!hasButton && <span>Total:</span>}
+            <s.Total>{total}</s.Total>
+            {hasButton && (
+              <Link href="/cart">
+                <Button as="a">Buy it now</Button>
+              </Link>
+            )}
+          </s.Footer>
+        </>
+      ) : (
+        <Empty
+          title="Your cart is empty"
+          description="Go back to the store and explore great games and offers"
+          hasLink
+        />
+      )}
+    </s.Wrapper>
+  )
+}
 export default CartList
