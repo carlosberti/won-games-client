a8bc4e0ffc0f03db58cfefe46da2473795274700
Carlos Berti <carlosberti182@gmail.com>
2021-03-31 02:58:55 -0300

    fix: add game id an fix styles

--- src/components/GameItem/index.tsx
+++ src/components/GameItem/index.tsx
@@ -1,4 +1,5 @@
 import { Download } from '@styled-icons/boxicons-solid/Download'
+import { useCart } from 'hooks/use-cart'
 
 import * as s from './styles'
 
@@ -10,6 +11,7 @@ export type PaymentInfoProps = {
 }
 
 export type GameItemProps = {
+  id: string
   img: string
   title: string
   price: string
@@ -18,43 +20,53 @@ export type GameItemProps = {
 }
 
 const GameItem = ({
+  id,
   img,
   title,
   price,
   downloadLink,
   paymentInfo
-}: GameItemProps) => (
-  <s.Wrapper>
-    <s.GameContent>
-      <s.ImageBox>
-        <img src={img} alt={title} />
-      </s.ImageBox>
+}: GameItemProps) => {
+  const { isInCart, removeFromCart } = useCart()
 
-      <s.Content>
-        <s.Title>
-          {title}
-          {!!downloadLink && (
-            <s.DownloadLink
-              href={downloadLink}
-              target="_blank"
-              aria-label={`Get ${title} here`}
-            >
-              <Download size={22} />
-            </s.DownloadLink>
-          )}
-        </s.Title>
-        <s.Price>{price}</s.Price>
-      </s.Content>
-    </s.GameContent>
-    {!!paymentInfo && (
-      <s.PaymentContent>
-        <p>{paymentInfo.purchaseDate}</p>
-        <s.CardInfo>
-          <span>{paymentInfo.number}</span>
-          <img src={paymentInfo.img} alt={paymentInfo.flag} />
-        </s.CardInfo>
-      </s.PaymentContent>
-    )}
-  </s.Wrapper>
-)
+  return (
+    <s.Wrapper>
+      <s.GameContent>
+        <s.ImageBox>
+          <img src={img} alt={title} />
+        </s.ImageBox>
+
+        <s.Content>
+          <s.Title>
+            {title}
+            {!!downloadLink && (
+              <s.DownloadLink
+                href={downloadLink}
+                target="_blank"
+                aria-label={`Get ${title} here`}
+              >
+                <Download size={22} />
+              </s.DownloadLink>
+            )}
+          </s.Title>
+          <s.Group>
+            <s.Price>{price}</s.Price>
+            {isInCart(id) && (
+              <s.Remove onClick={() => removeFromCart(id)}>Remove</s.Remove>
+            )}
+          </s.Group>
+        </s.Content>
+      </s.GameContent>
+      {!!paymentInfo && (
+        <s.PaymentContent>
+          <p>{paymentInfo.purchaseDate}</p>
+          <s.CardInfo>
+            <span>{paymentInfo.number}</span>
+            <img src={paymentInfo.img} alt={paymentInfo.flag} />
+          </s.CardInfo>
+        </s.PaymentContent>
+      )}
+    </s.Wrapper>
+  )
+}
 export default GameItem
