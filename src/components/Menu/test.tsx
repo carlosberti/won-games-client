e774bf44ae889498f3599e114a03752e8cecdc2c
Carlos Berti <carlosberti182@gmail.com>
2021-04-03 04:26:24 -0300

    test: fix menu flicker when user is logged and refresh/enter on a page

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -50,4 +50,11 @@ describe('<Menu />', () => {
     expect(screen.queryByText(/Sign in/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
   })
+
+  it('should not show signin nor dropdownUser if loading', () => {
+    render(<Menu username="Carlos" loading />)
+
+    expect(screen.queryByText(/my profile/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign in/i)).not.toBeInTheDocument()
+  })
 })
