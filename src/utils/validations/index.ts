d0853beeb5e3b35146a84bfb0bb3e92f90f945d7
Carlos Berti <carlosberti182@gmail.com>
2021-04-16 03:28:49 -0300

    feat: create reset and forgot password validates

--- src/utils/validations/index.ts
+++ src/utils/validations/index.ts
@@ -45,3 +45,26 @@ export function signInValidate(values: SignInValidate) {
 
   return getFieldErrors(schema.validate(values, { abortEarly: false }))
 }
+
+type ForgotValidateParams = Pick<UsersPermissionsRegisterInput, 'email'>
+
+export function forgotValidate(values: ForgotValidateParams) {
+  const { email } = fieldsValidations
+
+  const schema = Joi.object({ email })
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
+
+type ResetValidateParams = {
+  password: string
+  confirmPassword: string
+}
+
+export function resetValidate(values: ResetValidateParams) {
+  const { password, confirm_password } = fieldsValidations
+
+  const schema = Joi.object({ password, confirm_password })
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
