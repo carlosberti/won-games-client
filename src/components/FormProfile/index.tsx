805fd6200036b98ecb7e8fc39f60f838e8c2fc9c
Carlos Berti <carlosberti182@gmail.com>
2021-05-04 01:05:09 -0300

    feat: remove new passrod and confirm password field

--- src/components/FormProfile/index.tsx
+++ src/components/FormProfile/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import Button from 'components/Button'
 import Heading from 'components/Heading'
 import TextField from 'components/TextField'
@@ -29,19 +31,15 @@ const FormProfile = ({ email, username }: FormProfileProps) => (
         label="E-mail"
         disabled
       />
-      <TextField
-        name="password"
-        type="password"
-        placeholder="Type your password"
-        label="Password"
-      />
-      <TextField
-        name="new_password"
-        type="password"
-        placeholder="New password"
-        label="New password"
-      />
-      <Button size="large">Save</Button>
+
+      <s.ButtonContainer>
+        <Link href={`/forgot-password?email=${email}`} passHref>
+          <Button minimal size="medium" as="a">
+            Reset Password
+          </Button>
+        </Link>
+        <Button size="medium">Save</Button>
+      </s.ButtonContainer>
     </s.Form>
   </>
 )
